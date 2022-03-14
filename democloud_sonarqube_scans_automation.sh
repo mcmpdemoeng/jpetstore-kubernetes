36ae8b1ca9b1047983a4466b621ada66c9361bdd
mcmpdemo <70347050+mcmpdemo@users.noreply.github.com>
2022-03-14 08:19:49 -0600

    Fix maven version

--- democloud_sonarqube_scans_automation.sh
+++ democloud_sonarqube_scans_automation.sh
@@ -4,14 +4,15 @@ wget -q https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-sc
 unzip -qq -o sonar-scanner-cli-4.6.2.2472-linux.zip
 rm sonar-scanner-cli-4.6.2.2472-linux.zip
 
-wget -q https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
-tar -xzf apache-maven-3.8.4-bin.tar.gz
-rm apache-maven-3.8.4-bin.tar.gz
-./apache-maven-3.8.4/bin/mvn --version
+wget -q https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz
+tar -xzf apache-maven-3.8.5-bin.tar.gz
+rm apache-maven-3.8.5-bin.tar.gz
+./apache-maven-3.8.5/bin/mvn --version
 
 echo run scans
 
 SONAR_HOST=$(echo $DEMOCLOUD_SONARQUBE_HOST | base64 -d)
+#SONARQUBE_TOKEN=$(echo $SONARQUBE_DEMOCLOUD_TOKEN | base64 -d)
 
 cd angular
 ../sonar-scanner-4.6.2.2472-linux/bin/sonar-scanner -Dsonar.projectKey=angular-ui -Dsonar.sources=. -Dsonar.host.url=${SONAR_HOST} -Dsonar.login=${SONARQUBE_DEMOCLOUD_TOKEN}
@@ -24,4 +25,4 @@ cd external-issues
 cd ..
 
 cd security
-../apache-maven-3.8.4/bin/mvn clean verify sonar:sonar -Dsonar.projectKey=Democloud---Java-Sample-Project -Dsonar.host.url=${SONAR_HOST} -Dsonar.login=${SONARQUBE_DEMOCLOUD_TOKEN}
+../apache-maven-3.8.5/bin/mvn clean verify sonar:sonar -Dsonar.projectKey=Democloud---Java-Sample-Project -Dsonar.host.url=${SONAR_HOST} -Dsonar.login=${SONARQUBE_DEMOCLOUD_TOKEN}
