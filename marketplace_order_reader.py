aad7992ae559a451fb5fbe44c84e2d38ed82c3fa
Jose Pablo Ramirez Mendez <jose.ramirez.mendez1@kyndryl.com>
2022-04-28 14:57:25 -0600

    NEW: now reading order number to retrieve service inv id

--- marketplace_order_reader.py
+++ marketplace_order_reader.py
@@ -2,21 +2,37 @@ import requests
 import base64
 import os
 
-def get_order_number_details(tenant_system_user_name,tenant_system_user_api_key,service_instance_id,tenant_api_url):
+def get_order_number_details(tenant_system_user_name,tenant_system_user_api_key,order_number,tenant_api_url):
+    
+    data = {}
+
+    ENDPOINT = "{0}/v5/api/orders/{1}/detail".format(tenant_api_url, order_number)
+    headers = { 'username': tenant_system_user_name, 'apikey': tenant_system_user_api_key}
+    response = requests.get(url=ENDPOINT, headers=headers)
+    response = response.json()
+
+    data["service_instance_id"] = response["orderItems"][0]["services"][0]["serviceInventoryId"]
+
+    return data
+
+def get_service_details(tenant_system_user_name,tenant_system_user_api_key,service_instance_id,tenant_api_url):
     ENDPOINT = "{0}v3/api/services/azure/{1}".format(tenant_api_url, service_instance_id)
     headers = { 'username': tenant_system_user_name, 'apikey': tenant_system_user_api_key}   
     response = requests.get(url=ENDPOINT, headers=headers)
     return response.json()
 
 if __name__ == "__main__":
-    print("Script is working...")
 
     tenant_system_user_name = os.getenv("tenant_system_user_name")
     tenant_system_user_api_key = os.getenv("tenant_system_user_api_key")
-    service_instance_id = os.getenv("service_instance_id")
+    order_number = os.getenv("order_number")
     tenant_api_url = os.getenv("tenant_api_url")
 
-    order_details = get_order_number_details(tenant_system_user_name,tenant_system_user_api_key,service_instance_id,tenant_api_url)
+    order_details = get_order_number_details(tenant_system_user_name,tenant_system_user_api_key,order_number,tenant_api_url)
+    
+    service_instance_id = order_details["service_instance_id"]
+    
+    service_details = get_service_details(tenant_system_user_name,tenant_system_user_api_key,service_instance_id,tenant_api_url)
 
     fqdn = None
     kubeconfig = None
@@ -24,7 +40,7 @@ if __name__ == "__main__":
     db_user = None
     db_server_name = None
 
-    for r in order_details["resources"]:
+    for r in service_details["resources"]:
 
         # Get kubeconfig infor and FQDN
         if r["resourceType"] == "Microsoft.ContainerService/ManagedClusters":
@@ -57,3 +73,5 @@ if __name__ == "__main__":
         tmp_file = open(file_names[i], "w")
         tmp_file.write(file_contents[i])
         tmp_file.close()
+
+# petstorAKSDBJP
