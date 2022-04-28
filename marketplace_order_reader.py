f71f9a44a1e85f27a3380b98cdfd2c612ae911b4
Jose Pablo Ramirez Mendez <jose.ramirez.mendez1@kyndryl.com>
2022-04-28 08:14:42 -0600

    NEW: adding logic to read order details

--- marketplace_order_reader.py
+++ marketplace_order_reader.py
@@ -2,5 +2,20 @@ import requests
 import json
 import os
 
+def get_order_number_details(tenant_system_user_name,tenant_system_user_api_key,petstore_order_number,tenant_api_url):
+    ENDPOINT = "{0}v3/api/services/azure/{1}".format(tenant_api_url, petstore_order_number)
+    headers = { 'username': tenant_system_user_name, 'apikey': tenant_system_user_api_key}   
+    response = requests.get(url=ENDPOINT, headers=headers)
+    return response.json()
+
 if __name__ == "__main__":
     print("Script is working...")
+
+    tenant_system_user_name = os.getenv("tenant_system_user_name")
+    tenant_system_user_api_key = os.getenv("tenant_system_user_api_key")
+    petstore_order_number = os.getenv("petstore_order_number")
+    tenant_api_url = os.getenv("tenant_api_url")
+
+    order_details = get_order_number_details(tenant_system_user_name,tenant_system_user_api_key,petstore_order_number,tenant_api_url)
+
+    print(order_details)
