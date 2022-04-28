06b99f447528d87b3848d697ecb2b8f0b3cc8a08
Jose Pablo Ramirez Mendez <jose.ramirez.mendez1@kyndryl.com>
2022-04-28 10:06:23 -0600

    UPDATE: renaming order number arg

--- marketplace_order_reader.py
+++ marketplace_order_reader.py
@@ -2,8 +2,8 @@ import requests
 import base64
 import os
 
-def get_order_number_details(tenant_system_user_name,tenant_system_user_api_key,petstore_order_number,tenant_api_url):
-    ENDPOINT = "{0}v3/api/services/azure/{1}".format(tenant_api_url, petstore_order_number)
+def get_order_number_details(tenant_system_user_name,tenant_system_user_api_key,service_instance_id,tenant_api_url):
+    ENDPOINT = "{0}v3/api/services/azure/{1}".format(tenant_api_url, service_instance_id)
     headers = { 'username': tenant_system_user_name, 'apikey': tenant_system_user_api_key}   
     response = requests.get(url=ENDPOINT, headers=headers)
     return response.json()
@@ -13,10 +13,10 @@ if __name__ == "__main__":
 
     tenant_system_user_name = os.getenv("tenant_system_user_name")
     tenant_system_user_api_key = os.getenv("tenant_system_user_api_key")
-    petstore_order_number = os.getenv("petstore_order_number")
+    service_instance_id = os.getenv("service_instance_id")
     tenant_api_url = os.getenv("tenant_api_url")
 
-    order_details = get_order_number_details(tenant_system_user_name,tenant_system_user_api_key,petstore_order_number,tenant_api_url)
+    order_details = get_order_number_details(tenant_system_user_name,tenant_system_user_api_key,service_instance_id,tenant_api_url)
 
     fqdn = None
     kubeconfig = None
